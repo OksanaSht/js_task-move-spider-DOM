df71587c209c706d0550059efdfbad3322ac89a2
Oksana Shtelma <oshtelma@gmail.com>
2026-04-24 13:35:35 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,46 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  spider.style.boxSizing = 'border-box';
+
+  // Получаем параметры элемента (его позицию на экране)
+  const coord = wall.getBoundingClientRect();
+  const spiderSize = spider.getBoundingClientRect();
+
+  // eslint-disable-next-line no-console
+  // console.log(wall.style.borderWidth);
+
+  // Вычитаем позицию элемента из координат клика в окне
+  let xc = e.clientX - coord.left - spiderSize.width / 2;
+  let yc = e.clientY - coord.top - spiderSize.height / 2;
+
+  // eslint-disable-next-line no-console
+  // console.log(xc, yc);
+
+  if (xc <= 0) {
+    spiderSize.left = 0;
+  }
+
+  const rightEdge = wall.clientWidth - spiderSize.width;
+
+  if (xc > rightEdge) {
+    xc = rightEdge;
+  }
+
+  if (yc < 0) {
+    spiderSize.top = 0;
+  }
+
+  const bottomEdge = wall.clientHeight - spiderSize.height;
+
+  if (yc > bottomEdge) {
+    yc = bottomEdge;
+  }
+
+  spider.style.position = 'absolute';
+  spider.style.left = xc + 'px';
+  spider.style.top = yc + 'px';
 });
